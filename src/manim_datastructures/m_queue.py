10b919d1ca9a5aa2da97ab983fcf9563d48fdcc5
Ole skole <oleskole@eduroam-193-157-171-190.wlan.uio.no>
2023-11-27 14:43:37 +0100

    fikset queuen

--- src/manim_datastructures/m_queue.py
+++ src/manim_datastructures/m_queue.py
@@ -4,16 +4,17 @@ from manim import *
 class m_queue(VGroup):
     def __init__(self, queue=[], title="Queue", scale=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.len = ValueTracker(len(queue))
         self.scale = scale
-        self.queue = queue
         self.title = Text(title)
         self.start_line = Line((0, 0, 0), (0, 1, 0)).scale(self.scale).next_to(self.title, RIGHT)
-        self.prev = self.start_line
+        self.queue = [self.start_line]
+        self.prev = self.queue[-1]
+
 
         self.add(self.title, self.start_line)
         for i in queue:
             self.enqueue_inner(i)
+        
         
 
     
@@ -26,7 +27,15 @@ class m_queue(VGroup):
         return result.scale(self.scale)
 
     def enqueue_inner(self, value):
-        new_elem = self.create_textbox(str(value), 30).next_to(self.prev, RIGHT, buff=0)
+        new_elem = self.create_textbox(str(value), 30).next_to(self.queue[-1], RIGHT, buff=0)
+        
+        # def update_func(m):
+        #     for i in range(len(self.queue)):
+        #         if self.queue[i] == m:
+        #             m.next_to(self.queue[i-1], RIGHT, buff=0)
+        #             return   # Exit the loop.
+
+        # new_elem.add_updater(update_func)
         self.add(new_elem)
         self.queue.append(new_elem)
         self.prev = self.queue[-1]
@@ -43,49 +52,26 @@ class m_queue(VGroup):
             **kwargs)
     
     def dequeue(self, **kwargs):
-        if len(self.queue) == 0:
+        if len(self.queue) <= 1:
             return Succession(**kwargs)
         anims = []
-        anim_group = []
-        anims.append(FadeOut(self.queue[0], shift=LEFT))
-        anims.append(self.queue[1].animate.next_to(self.start_line, RIGHT))
-        anims.append(AnimationGroup(*anim_group, **kwargs))
+        anims.append(FadeOut(self.queue[1], shift=LEFT))
         anims.append(Wait())
-        self.remove(self.queue.pop(0))
-        self.len -= 1
-        return LaggedStart(*anims, **kwargs)
+        for i in range(2, len(self.queue)):
+            anims.append(self.queue[i].animate.next_to(self.queue[i-2], RIGHT, buff=0))
+        self.remove(self.queue.pop(1))
+        return AnimationGroup(*anims, **kwargs)
 
     def fade_out(self, **kwargs):
         anims = []
         for elem in self:
             anims.append(FadeOut(elem))
         anims.append(Wait())
-        return LaggedStart(*anims, **kwargs)      
+        return AnimationGroup(*anims, **kwargs)      
     
     def fade_in(self, **kwargs):
         anims = []
         for elem in self:
             anims.append(FadeIn(elem))
         anims.append(Wait())
-        return LaggedStart(*anims, **kwargs)
-
-
-if __name__ == "__main__":
-    class theScene(Scene):
-        def construct(self):
-            queue = m_queue(queue=[1, 2, 3, 4, 5], title="Queue")
-            self.add(queue)
-            self.play(queue.enqueue(6, run_time=2, rate_func=linear))
-            self.wait()
-            self.play(queue.dequeue(run_time=2, rate_func=linear))
-            self.wait()
-            self.play(queue.fade_out(run_time=2, rate_func=linear))
-            self.wait()
-            self.play(queue.fade_in(run_time=2, rate_func=linear))
-            self.wait()
-    
-    import os
-    os.system("manim -p -ql ./m_queue.py theScene")
-    input("Press Enter to continue...")
-    os.system("rm -rf ./__pycache__")
-    os.system("rm -rf ./media")
+        return AnimationGroup(*anims, **kwargs)
