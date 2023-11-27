3a54b0d3827b77678bac582c85531e517529cd8b
Ole skole <oleskole@eduroam-193-157-171-190.wlan.uio.no>
2023-11-27 16:09:05 +0100

    fikset bug i table

--- src/manim_datastructures/m_table.py
+++ src/manim_datastructures/m_table.py
@@ -20,9 +20,10 @@ class m_table(VGroup):
         self.con_table = [[s, f"0x{n:03x}"] for s, n in self.data.items()]
         self.table = Table(self.con_table, include_outer_lines=True).scale(self.scale)
         self.table_title.next_to(self.table, UP)
-        self.become(VGroup(self.table, self.table_title))
+        self.become(VGroup(self.table, self.table_title).move_to(self.get_center()))
     
     def animate_change(self, register, value, **kwargs):
+        self.update_table()
         anims = []
         index = list(self.data.keys()).index(register)+1
         self.table.add_highlighted_cell((index, 0), color=GREEN)
