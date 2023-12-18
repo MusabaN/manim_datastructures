3b3ad12c3f48f50891eb099adb29dca92113f7ee
Ole Jørgen Norstrand <olejno@uio.no>
2023-12-18 13:50:32 +0100

    refactored memory module to be abstract
    also created a map module, so you can color between a memory module and a map module.

--- src/manim_datastructures/m_map.py
+++ src/manim_datastructures/m_map.py
@@ -1,13 +1,19 @@
 from manim import *
 import numpy as np
+from .m_abstract_memory import m_abstract_memory
 
 
-class m_map(VGroup):
-     def __init__(self, height=2, width=2, num_cells = 4):
-        super().__init__()
+class m_map(m_abstract_memory):
+    def __init__(self, height=2, width=2, num_cells = 4):
+        super().__init__(height, width, num_cells)
 
         self.text = Text("Map")
-        self.rect = Rectangle(height=height, width=width).surround(self.text, buff=2)
+        self.rect.surround(self.text, buff=2)
         self.num_cells = num_cells
 
-        self.add(self.rect, self.text)
+        self.start = self.rect.get_corner(UL)
+        self.end = self.rect.get_corner(DL)
+
+        self.between = (self.end - self.start) / self.num_cells
+
+        self.add(self.text)
--- src/manim_datastructures/m_memory.py
+++ src/manim_datastructures/m_memory.py
@@ -1,58 +1,16 @@
 from manim import *
 import numpy as np
+from .m_abstract_memory import m_abstract_memory
 
-class m_memory(VGroup):
+class m_memory(m_abstract_memory):
     
-    def __init__(self, height=5, width=2, num_cells = 4):
-        super().__init__()
+    def __init__(self, height=5, width=2, num_cells=4):
+        super().__init__(height, width, num_cells)
 
-        self.rect = Rectangle(height=height, width=width)
-        self.num_cells = num_cells
-
-        start = self.rect.get_corner(UL)
-        end = self.rect.get_corner(DL)
-
-        between = (end - start) / num_cells
-        
-        start += between
+        self.start = self.start + self.between
 
         for _ in range(num_cells):
-            self.add(Line(start, (start + (RIGHT * width))))
-            start += between
+            self.add(Line(self.start, (self.start + (RIGHT * width))))
+            self.start += self.between
         
-        self.between = between
-
-        self.add(self.rect)
-    
-
-    def get_cell_position(self, cell_num, pos):
-        if np.array_equal(pos, LEFT):
-            return (self.rect.get_corner(UL) + (self.between * cell_num))
-        else:
-            return (self.rect.get_corner(UR) + (self.between * cell_num))
-    
-    def color_between(self, other_memory: 'm_memory', color: 'manim_colors', start_1, start_2, distance, direction=RIGHT):
-        dir1, dir2 = LEFT, RIGHT
-        if np.array_equal(direction, RIGHT):
-            dir1, dir2 = RIGHT, LEFT
-        
-        polys = VGroup()
-        for i, j in zip(range(start_1, start_1 + distance), range(start_2, start_2 + distance)):
-            s1 = self.get_cell_position(i, dir1)
-            e1 = self.get_cell_position(i+1, dir1)
-            s2 = other_memory.get_cell_position(j, dir2)
-            e2 = other_memory.get_cell_position(j+1, dir2)
-
-            color_bounds = color
-
-            if i < 0 or i >= self.num_cells:
-                color_bounds = RED
-            if j < 0 or j >= other_memory.num_cells:
-                color_bounds = RED
-            
-            polys.add(Polygon(s1, e1, e2, s2,
-                              color=color_bounds, 
-                    fill_opacity=0.2,
-                    stroke_width=0))
-            
-        return polys
+        self.between = self.between
