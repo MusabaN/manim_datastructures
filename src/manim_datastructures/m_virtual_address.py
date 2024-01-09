7db4d9db621a6cf21b0329873940f8ea9b8a9db5
Ole Jørgen Norstrand <olejno@uio.no>
2024-01-09 12:31:00 +0700

    virtual address complete

--- src/manim_datastructures/m_virtual_address.py
+++ src/manim_datastructures/m_virtual_address.py
@@ -1,15 +1,61 @@
 from manim import *
 
+
+class m_virtual_address_element(VGroup):
+    def __init__(self, num):
+        super().__init__()
+        self.num = num
+
+        self.square = Square(1)
+        self.integer = Integer(num).move_to(self.square.get_center())
+
+        self.add(Square(1), Integer(num))
+
+    def highlight(self, color):
+        self[0].set_fill(color, opacity=0.5)
+
+    def unhighlight(self):
+        self[0].set_fill(BLACK, opacity=0)
+
+
 class m_virtual_address(VGroup):
     
     def __init__(self, arr):
         super().__init__()
 
-        arr_ints = [Integer(i) for i in arr]
-        arr_rects = [Square(1) for i in arr]
+        elems = [m_virtual_address_element(i) for i in arr]
 
-        for i in range(1, len(arr_rects)):
-            arr_rects[i].next_to(arr_rects[i-1], RIGHT, buff=0)
-            arr_ints[i].move_to(arr_rects[i].get_center())
+        for i in range(1, len(elems)):
+            elems[i].next_to(elems[i-1], RIGHT, buff=0)
 
-        self.add(*arr_rects, *arr_ints)
+        self.add(*elems)
+
+    def highlight(self, index, color):
+        self[index].highlight(color)
+
+    def unhighlight(self, index):
+        self[index].unhighlight()
+
+
+    def multi_highlight(self, start, end, color):
+        for i in range(start, end):
+            self.highlight(i, color)
+
+    def multi_unhighlight(self, start, end):
+        for i in range(start, end):
+            self.unhighlight(i)
+
+    # make indices version of multihighlight
+    def multi_highlight_indices(self, indices, color):
+        for i in indices:
+            self.highlight(i, color)
+
+    def multi_unhighlight_indices(self, indices):
+        for i in indices:
+            self.unhighlight(i)
+
+    def popup_element(self, idx):
+        self[idx].move_to(self[idx].get_center()+UP*0.25)
+
+    def popdown_element(self, idx):
+        self[idx].move_to(self[idx].get_center()+DOWN*0.25)
--- test/virtual_address_test.py
+++ test/virtual_address_test.py
@@ -8,3 +8,11 @@ class theScene(Scene):
         va = m_virtual_address([1, 2, 3, 4, 5])
 
         self.add(va)
+        
+        self.play(va.animate.highlight(1, GREEN).popup_element(1))
+        self.wait()
+
+        self.play(va.animate.popdown_element(1).unhighlight(1))
+        self.wait()
+
+        
